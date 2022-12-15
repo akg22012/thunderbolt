d42d362a64115a93396e3207a9d6f2a8f7a99063
Razrcorp <prathamesh.palkar@razrcorp.com>
2022-12-15 18:07:08 +0530

    new pages add

--- src/pages/documentation.jsx
+++ src/pages/documentation.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Layout from "../components/Layout";
 
-const terms = () => {
+const Documentation = () => {
   return (
     <Layout>
       <section className="commingSoon">
@@ -9,20 +9,33 @@ const terms = () => {
           <div className="row">
             <div className="col-12">
               <div className="commingSoonHeader">
-                <h1>coming 
-<svg id="Group_20978" data-name="Group 20978" xmlns="http://www.w3.org/2000/svg" width="59.761" height="82.367" viewBox="0 0 59.761 82.367">
-  <path id="Path_104553" data-name="Path 104553" d="M449.35,607.561h25.713l-22.887,27.691,13.281-.848-24.865,21.757,11.726-1.305L415.3,689.927l12.574-21.475-6.358,1.13,15.3-24.022-6.254.993Z" transform="translate(-415.302 -607.561)" fill="#f7df00"/>
-</svg>
- soon...</h1>
+                <h1>
+                  coming
+                  <svg
+                    id="Group_20978"
+                    data-name="Group 20978"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="59.761"
+                    height="82.367"
+                    viewBox="0 0 59.761 82.367"
+                  >
+                    <path
+                      id="Path_104553"
+                      data-name="Path 104553"
+                      d="M449.35,607.561h25.713l-22.887,27.691,13.281-.848-24.865,21.757,11.726-1.305L415.3,689.927l12.574-21.475-6.358,1.13,15.3-24.022-6.254.993Z"
+                      transform="translate(-415.302 -607.561)"
+                      fill="#f7df00"
+                    />
+                  </svg>
+                  soon...
+                </h1>
               </div>
             </div>
           </div>
-         
         </div>
-        
       </section>
     </Layout>
   );
 };
 
-export default terms;
+export default Documentation;
